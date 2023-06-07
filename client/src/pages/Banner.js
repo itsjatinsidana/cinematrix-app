bb6dcc1eeeb358f9a0273fe978034ab3f824eae3
jatin sidana <jatinsadana1998@gmail.com>
2023-06-07 22:10:17 +0530

    banner updated

--- client/src/pages/Banner.js
+++ client/src/pages/Banner.js
@@ -1,12 +1,16 @@
 export const Banner = () =>{
     return(
         <>
-        <div className="banner-container">
-            <img src="../assets/images/banner.jpg" alt="banner-image" className="banner-image"/>
-            <h3 className="overlay-text">movie name</h3>
+        <div className="banner-container" > 
+    
+           
+          <h3 className="overlay-text">Movie name</h3>
             <p className="overlay-para">movie description in 2 lines</p>
             <button className="btn-clr">Watch Trailer</button>
-             </div>
+          
+            </div> 
+             
+            
         </>
     )
 }
