86d7e7e7037549fba058cd5371f8f72caeb6091e
jatin sidana <jatinsadana1998@gmail.com>
2023-06-07 23:32:53 +0530

    movie cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,15 @@
  import{PublicNavbar} from "../components/PublicNavbar";
  import{Banner} from "../pages/Banner";
  import{ Footer } from "../components/Footer";
+import{MovieCard} from "../pages/MovieCard";
  export const Home = ()=>{
     return(
         <>
         <div className="home-container">
                 <PublicNavbar/>
                    <Banner/>
-            
-        <div className="movie-section-container">
-                     movie section
-        </div>
+                   <MovieCard/>
+        
        <Footer/>
         </div>
         </>
