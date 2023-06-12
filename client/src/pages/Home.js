79db149497d10a264fad8ea7d196399d6b33f6f5
jatin sidana <jatinsadana1998@gmail.com>
2023-06-12 18:04:39 +0530

    tvshows card updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,11 @@ import { PublicNavbar } from "../components/PublicNavbar";
 import { Banner } from "../pages/Banner";
 import { Footer } from "../components/Footer";
 import axios from "axios";
-
-import { ShowsCard } from "../pages/ShowsCard";
 import { useEffect, useState } from "react";
+
 export const Home = () => {
     const [movieCard, setMovieCard] = useState([]);
+    const [showCard, setShowCard] = useState([]);
     async function getMovieCard() {
         const options = {
             method: 'GET',
@@ -30,6 +30,34 @@ export const Home = () => {
         getMovieCard()
     }, [])//[] dependency arr used to depend on a state which we put insite that dependency arr
 
+    {/*get shows card function */ }
+    async function getTvShows() {
+
+        const options = {
+            method: 'GET',
+            url: 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming',
+            params: {
+                limit: '3',
+                page: '2'
+            },
+            headers: {
+                'X-RapidAPI-Key': '904536df4emsh2aac700c28b8eedp1660adjsn45c008306c57',
+                'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data.results);
+            setShowCard(response.data.results)
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+    useEffect(() => {
+        getTvShows();
+    }, [])
 
     return (
 
@@ -43,6 +71,7 @@ export const Home = () => {
                     <h1>Popular movies</h1>
                     <button className="btn-trailer">explore more</button>
                 </div>
+                <br/>
                 {
                     movieCard.map(((value, index) => {
                         return (
@@ -60,8 +89,31 @@ export const Home = () => {
                         )
                     }))
                 }
+                {/* popular shows section*/}
 
-                <ShowsCard />
+                <div className="popular-container" >
+                    <h1>Popular Tvshows</h1>
+                    <button className="btn-trailer">Explore more</button>
+                </div>
+               <br/>
+                {
+                    showCard.map(((value, index) => {
+                        return (
+                            <>
+                                <div className="card" key={index}>
+                                    <div className="card-image">
+                                    <img src={value.primaryImage?value.primaryImage.url:"../images/spider.jpg.jpg"} alt="Card Image" />
+                                    </div>
+                                    <div className="card-content">
+                                        <h3>{value.originalTitleText.text}</h3>
+                                        <p>{value.titleType.text}</p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }))
+                }
+
 
                 <Footer />
             </div>
